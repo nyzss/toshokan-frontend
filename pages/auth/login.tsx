1a43bf4235d992d879983b09be11577c12a9cde5
nyzs <neazenmeh@gmail.com>
2021-07-09 10:16:32 +0200

    redirects to "/" when user is logged in (from the login page)

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,7 +1,19 @@
 import Head from "next/head";
-import { LoginCard } from "../../components/Authentification/LoginCard";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { LoginCard } from "../../components/Authentification/Login/LoginCard";
+import { userStore } from "../../store/Store";
 
 const Login: React.FC = () => {
+  const router = useRouter();
+
+  const { user } = userStore();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
   return (
     <>
       <Head>
